7d9737bf02ea808644f127e669cb9c1d5916c7ea
Gülşah <69244976+arifegulsah@users.noreply.github.com>
2021-10-28 22:44:32 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+//FORM BUTONU HREF
+document.getElementById("show-form").onclick = function()
+{
+    location.href = "form.php";
+}
+
+//KOD CHECK BUTONU HREF
+document.getElementById("show-check").onclick = function()
+{
+    location.href = "check.html";
+}
+
+
+
+
+
+
+
